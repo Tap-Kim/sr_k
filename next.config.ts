646970c6daa2dac69fa4860f1cf7ce189576ad2c
devTap <rlaxogus0517@gmail.com>
2025-09-03 23:44:55 +0900

    Update .gitignore and next.config.ts: add .next and out to .gitignore, modify basePath format

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/sr_k/",
+  basePath: "/sr_k",
   assetPrefix: "/sr_k/",
   images: {
     unoptimized: true,
